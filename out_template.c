7f96da58afbea733a447e4e91f58cbc239ac083b
yuki tabei <yukkidohno632@gmail.com>
2024-04-22 00:49:46 +0900

    end

--- out_template.c
+++ out_template.c
@@ -12,15 +12,15 @@ int main()
         if(f != NULL)
         {
             fprintf(f,
-                "X_Mesh_Sizes:1, 2, 3\n"
-                "Y_Mesh_Sizes:1, 2, 3\n"
-                "Z_Mesh_Sizes:1, 2, 3\n"
+                "X_Mesh_Sizes(1,2,3)\n"
+                "Y_Mesh_Sizes(1,2,3)\n"
+                "Z_Mesh_Sizes(1,2,3)\n"
                 "Column : Span() Width() Depth() X_Center() Y_Center()\n"
                 "Beam   : Span() Width() Depth() Y_Center() Z_Center()\n"
                 "xBeam  : Width()\n"
-                "Rebar 1: X=1 Y=1\n"
-                "Rebar 2: X=1 Y=1\n"
-                "Rebar 3: X=1 Y=1\n"
+                "Rebar 1: X() Y()\n"
+                "Rebar 2: X() Y()\n"
+                "Rebar 3: X() Y()\n"
                 "END\n");
             fclose(f);
         }
